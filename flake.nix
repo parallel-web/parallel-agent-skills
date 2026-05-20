7252ee465dce8415a8729793d11b5ae2094b8e84
Serj Babayan <hello@serjb.com>
2026-05-19 17:36:40 -0700

    update pnpm

--- flake.nix
+++ flake.nix
@@ -13,9 +13,13 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+
+      pnpm = pkgs.writeShellScriptBin "pnpm" ''
+        exec corepack pnpm "$@"
+      '';
     in {
       devShells.default = pkgs.mkShell {
-        packages = [pkgs.nodejs_22 pkgs.pnpm pkgs.pre-commit];
+        packages = [pkgs.nodejs_25 pnpm pkgs.pre-commit];
       };
     });
 }
